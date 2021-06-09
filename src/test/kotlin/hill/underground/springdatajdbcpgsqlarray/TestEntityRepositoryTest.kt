d6273856cd313bd09736e3a1d931901b0ffc8a0a
Underground Hill <underground.hill@mailbox.org>
2021-06-09 22:13:39 +0200

    Make test a bit clearer

--- src/test/kotlin/hill/underground/springdatajdbcpgsqlarray/TestEntityRepositoryTest.kt
+++ src/test/kotlin/hill/underground/springdatajdbcpgsqlarray/TestEntityRepositoryTest.kt
@@ -17,7 +17,7 @@ class TestEntityRepositoryTest {
     @Test
     fun `should return correct items when fetching by id`() {
         // given
-        val items = listOf("first", "second")
+        val items = listOf("first", "second", "third")
         val entity = TestEntity(items = items)
         val persistedEntity = repository.save(entity)
 
@@ -25,17 +25,18 @@ class TestEntityRepositoryTest {
         val retrievedEntity = repository.findById(persistedEntity.id!!).orElseThrow()
 
         // then
-        Assertions.assertEquals(2, retrievedEntity.items.size)
+        Assertions.assertEquals(3, retrievedEntity.items.size)
         Assertions.assertEquals("first", retrievedEntity.items[0])
         Assertions.assertEquals("second", retrievedEntity.items[1])
+        Assertions.assertEquals("third", retrievedEntity.items[2])
     }
 
     @Test
     fun `should return correct items when fetching by using custom query`() {
         // given
-        val items = listOf("first", "second")
+        val items = listOf("first", "second", "third")
         val entity = TestEntity(items = items)
-        val persistedEntity = repository.save(entity)
+        repository.save(entity)
 
         // when
         val retrievedItemCombinations = repository.findDistinctItemCombinations()
@@ -43,8 +44,10 @@ class TestEntityRepositoryTest {
         // then
         Assertions.assertAll(
             Executable { Assertions.assertEquals(1, retrievedItemCombinations.size) },
-            Executable { Assertions.assertEquals("first", retrievedItemCombinations.first()[0]) },
-            Executable { Assertions.assertEquals("second", retrievedItemCombinations.first()[1]) }
+            Executable { Assertions.assertEquals(3, retrievedItemCombinations.first().size) },
+            Executable { Assertions.assertEquals("first", retrievedItemCombinations.first()[0]) },  // is "{first"
+            Executable { Assertions.assertEquals("second", retrievedItemCombinations.first()[1]) },
+            Executable { Assertions.assertEquals("third", retrievedItemCombinations.first()[2]) }   // is "third}"
         )
     }
 }
